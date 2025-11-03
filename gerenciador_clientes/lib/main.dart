a6b53d2a45e96fe5028be4bfdd76a156850f7b67
vitor-segna <vitor.h.rocha9@aluno.senai.br>
2025-11-03 14:05:16 -0300

    commit mural

--- gerenciador_clientes/lib/main.dart
+++ gerenciador_clientes/lib/main.dart
@@ -32,25 +32,77 @@ class AplicativoClientes extends StatelessWidget{
  }
 }
 
-class TelaPrincipal extends StatelessWidget{
+
+
+//mural/TELA INICIAL
+class TelaPrincipal extends StatelessWidget {
 final Cliente cliente;
 
 const TelaPrincipal({super.key, required this.cliente});
 
 @override
-Widget build(BuildContext context){
- return Scaffold( //Tela
-  appBar: AppBar(title: const Text('Tela Principal (Em Construção)')),
-  body: Center(
-  child: Text(
-  'Bem Vindo, ${cliente.nome}!!',
-  style: const TextStyle(fontSize: 24),  
-  ),  
-  ),
-  );  
+Widget build(BuildContext context) {
+return Scaffold(
+appBar: AppBar(
+title: const Text('Área do Cliente'),
+automaticallyImplyLeading: false, // Remove a seta de voltar.
+actions: [
+// Botão de Sair (Logout).
+IconButton(
+icon: const Icon(Icons.logout),
+onPressed: () {
+// Navegação: Limpa a pilha e volta para a Tela de Login.
+Navigator.pushAndRemoveUntil(
+context,
+MaterialPageRoute(builder: (context) => const TelaLogin()),
+(Route<dynamic> route) => false, // Condição que remove todas as rotas.
+);
+},
+tooltip: 'Sair do Sistema',
+)
+],
+),
+body: Center(
+child: Padding(
+padding: const EdgeInsets.all(16.0),
+child: Column(
+mainAxisAlignment: MainAxisAlignment.center,
+children: [
+const Icon(Icons.check_circle_outline, size: 80, color: Colors.indigo),
+const SizedBox(height: 20),
+Text('Login de ${cliente.nome} realizado!', style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+const SizedBox(height: 8),
+Text('E-mail: ${cliente.email}', style: const TextStyle(fontSize: 18, color: Colors.grey)),
+const SizedBox(height: 40),
+// Título da lista de clientes
+const Text('Clientes cadastrados (BD Simulado):', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+const SizedBox(height: 10),
+// Lista de Clientes Cadastrados
+Expanded(
+// Usa o getter 'clientes' do nosso gerenciador.
+child: ListView.builder(
+itemCount: gerenciadorClientes.clientes.length,
+itemBuilder: (context, index) {
+final c = gerenciadorClientes.clientes[index];
+return ListTile(
+leading: const Icon(Icons.person),
+title: Text(c.nome),
+subtitle: Text(c.email),
+);
+},
+),
+),
+],
+),
+),
+),
+);
 }
 }
 
+
+
+//tela do login
 class TelaLogin extends StatelessWidget{
   const TelaLogin({super.key});
   @override
